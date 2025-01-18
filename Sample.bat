29d5536829e80848c5a87423b50a49b987939b78
Kshitija0110 <kshitudeshmukh3@gmail.com>
2025-01-18 13:58:02 +0530

    JCL backup 1-02_135648

--- Sample.bat
+++ Sample.bat
@@ -2,15 +2,15 @@
 echo Starting JCL backup and Git commit process...
 
 REM Set variables
-
-git config --global user.email "kshitudeshmukh3@gmail.com"
-git config --global user.name "Kshitija0110"
-git config --global --add safe.directory %REPO_DIR%
-
 set BACKUP_DIR=C:\JCLBackup
 set REPO_DIR=C:\GitRepo\jcl-backup
 set DATE_STAMP=%date:~10,4%%date:~4,2%%date:~7,2%
 set TIME_STAMP=%time:~0,2%%time:~3,2%%time:~6,2%
+
+REM Configure Git user
+git config --global user.email "kshitudeshmukh3@gmail.com"
+git config --global user.name "Kshitija0110"
+git config --global --add safe.directory %REPO_DIR%
 
 REM Create backup directory if it doesn't exist
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
@@ -19,35 +19,23 @@ REM Run FTP script to download JCL members
 echo Downloading JCL members...
 ftp -s:"%WORKSPACE%\SampleFTP.txt"
 
-REM Navigate to repo and perform git pull
+REM Navigate to repo and handle any existing merge
 cd "%REPO_DIR%"
 if exist ".git\MERGE_HEAD" (
     git merge --abort
 )
 
 echo Pulling latest changes from remote repository...
-git pull origin main
+git pull origin main --strategy-option theirs
 
 REM Copy files to Git repository
 echo Copying files to Git repository...
-if not exist "%REPO_DIR%" (
-    mkdir "%REPO_DIR%"
-    cd "%REPO_DIR%"
-    git init
-)
-
-REM Copy new files to repo directory
-echo Copying files to Git repository...
 xcopy /Y "%BACKUP_DIR%\*.*" "%REPO_DIR%"
 
-<<<<<<< HEAD
-REM Perform Git operation
-=======
 REM Perform Git operations
->>>>>>> a96d7144bf8d1a118a493a659415af60ecb41b97
 git add .
 git commit -m "JCL backup %DATE_STAMP%_%TIME_STAMP%"
 git push origin main
 
 echo Backup and commit complete!
-pause
+if defined JENKINS_HOME (exit 0) else pause
