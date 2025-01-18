a96d7144bf8d1a118a493a659415af60ecb41b97
Kshitija0110 <kshitudeshmukh3@gmail.com>
2025-01-18 11:57:48 +0530

    JCL backup 20250118_115747

--- Sample.bat
+++ Sample.bat
@@ -2,6 +2,11 @@
 echo Starting JCL backup and Git commit process...
 
 REM Set variables
+
+git config --global user.email "kshitudeshmukh3@gmail.com"
+git config --global user.name "Kshitija0110"
+git config --global --add safe.directory %REPO_DIR%
+
 set BACKUP_DIR=C:\JCLBackup
 set REPO_DIR=C:\GitRepo\jcl-backup
 set DATE_STAMP=%date:~10,4%%date:~4,2%%date:~7,2%
@@ -10,18 +15,16 @@ set TIME_STAMP=%time:~0,2%%time:~3,2%%time:~6,2%
 REM Create backup directory if it doesn't exist
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
 
-REM Configure Git to trust the directory when running as SYSTEM
-git config --global --add safe.directory %REPO_DIR%
-
 REM Run FTP script to download JCL members
 echo Downloading JCL members...
-ftp -s:SampleFTP.txt
+ftp -s:"%WORKSPACE%\SampleFTP.txt"
 
 REM Navigate to repo and perform git pull
-cd "%REPO_DIR%" || (
-    echo Error: Cannot access repository directory
-    exit /b 1
+cd "%REPO_DIR%"
+if exist ".git\MERGE_HEAD" (
+    git merge --abort
 )
+
 echo Pulling latest changes from remote repository...
 git pull origin main
 
